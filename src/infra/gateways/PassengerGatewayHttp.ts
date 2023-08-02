4d8a063cb447098290557a4633884b641faffd99
Matheus Rian <souzamatheusrian@gmail.com>
2023-08-02 20:11:06 -0300

    feat: returns passengerId when save passenger

--- src/infra/gateways/PassengerGatewayHttp.ts
+++ src/infra/gateways/PassengerGatewayHttp.ts
@@ -1,10 +1,12 @@
+import Passenger from "../../domain/passenger/Passenger";
 import PassengerGateway from "./PassengerGateway";
 import HttpClient from "./http/HttpClient";
 
 export default class PassengerGatewayHttp implements PassengerGateway {
   constructor (readonly httpClient: HttpClient) { }
 
-  async save(passenger: any): Promise<void> {
-    return await this.httpClient.post('http://localhost:3000/passengers', passenger);
+  async save(passenger: Passenger): Promise<string> {
+    const passengerData = await this.httpClient.post('http://localhost:3000/passengers', passenger);
+    return passengerData.passengerId;
   }
 }
