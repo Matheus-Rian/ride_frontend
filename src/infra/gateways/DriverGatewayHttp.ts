eadc80bad0486ddcb5149145bb533eea57938204
Matheus Rian <souzamatheusrian@gmail.com>
2023-08-02 20:27:06 -0300

    refactor: adjusts gateway driver

--- src/infra/gateways/DriverGatewayHttp.ts
+++ src/infra/gateways/DriverGatewayHttp.ts
@@ -1,10 +1,12 @@
+import Driver from "../../domain/Driver";
 import DriverGateway from "./DriverGateway";
 import HttpClient from "./http/HttpClient";
 
 export default class DriverGatewayHttp implements DriverGateway {
   constructor (readonly httpClient: HttpClient) { }
 
-  async save (driver: any) {
-    return await this.httpClient.post('http://localhost:3000/drivers', driver);
+  async save (driver: Driver) {
+    const driverData = await this.httpClient.post('http://localhost:3000/drivers', driver);
+    return driverData.driverId;
   }
 }
