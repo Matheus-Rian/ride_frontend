070755b162e51c3d57afdc53cdeceb803cf5801c
Matheus Rian <souzamatheusrian@gmail.com>
2023-08-02 22:24:24 -0300

    feat: create DTO input passenget

--- src/infra/gateways/PassengerGatewayHttp.ts
+++ src/infra/gateways/PassengerGatewayHttp.ts
@@ -1,12 +1,17 @@
 import Passenger from "../../domain/Passenger";
-import PassengerGateway from "./PassengerGateway";
+import PassengerGateway, { CreatePassengerInput } from "./PassengerGateway";
 import HttpClient from "./http/HttpClient";
 
 export default class PassengerGatewayHttp implements PassengerGateway {
   constructor (readonly httpClient: HttpClient) { }
 
-  async save(passenger: Passenger): Promise<string> {
-    const passengerData = await this.httpClient.post('http://localhost:3000/passengers', passenger);
+  async create(passenger: Passenger): Promise<string> {
+    const input: CreatePassengerInput = {
+      document: passenger.document.getValue(),
+      email: passenger.email.getValue(),
+      name: passenger.name.getValue()
+    }
+    const passengerData = await this.httpClient.post('http://localhost:3000/passengers', input);
     return passengerData.passengerId;
   }
 }
