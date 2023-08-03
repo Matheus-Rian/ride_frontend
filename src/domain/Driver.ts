45c205744951a5655a59fbb261e4a72b1a048631
Matheus Rian <souzamatheusrian@gmail.com>
2023-08-03 09:39:38 -0300

    feat: create DriverBuilder in domain

--- src/domain/Driver.ts
+++ src/domain/Driver.ts
@@ -1,8 +1,43 @@
+import CarPlate from "./CarPlate";
+import Cpf from "./Cpf";
+import Email from "./Email";
+import Name from "./Name";
+
 export default class Driver {
+  driverId: string;
+  name: Name; 
+  email: Email;
+  document: Cpf;
+  carPlate: CarPlate;
+
   constructor (
-    readonly name: string, 
-    readonly email: string, 
-    readonly document: string, 
-    readonly carPlate: string, 
-  ) { }
+    driverId: string,
+    name: string, 
+    email: string, 
+    document: string, 
+    carPlate: string, 
+  ) { 
+    this.driverId = driverId;
+    this.name = new Name(name);
+    this.email = new Email(email);
+    this.document = new Cpf(document);
+    this.carPlate = new CarPlate(carPlate);
+  }
+
+  static create(builder: DriverBuilder) {
+    const { driverId, name, email, document, carPlate } = builder;
+    return new Driver(driverId, name, email, document, carPlate);
+  }
+}
+
+export class DriverBuilder {
+  driverId = '';
+  name = ''; 
+  email = ''; 
+  document = ''; 
+  carPlate = ''; 
+
+  build() {
+    return Driver.create(this);
+  }
 }
