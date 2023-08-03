6efa5c923c0fc8d0087d3d1cf7b0626d66c9ef69
Matheus Rian <souzamatheusrian@gmail.com>
2023-08-03 09:40:35 -0300

    feat: using DTO in DriverGatewayHttp

--- src/infra/gateways/DriverGatewayHttp.ts
+++ src/infra/gateways/DriverGatewayHttp.ts
@@ -1,12 +1,19 @@
 import Driver from "../../domain/Driver";
-import DriverGateway from "./DriverGateway";
+import DriverGateway, { CreateDriverInput } from "./DriverGateway";
 import HttpClient from "./http/HttpClient";
 
 export default class DriverGatewayHttp implements DriverGateway {
   constructor (readonly httpClient: HttpClient) { }
 
-  async save (driver: Driver) {
-    const driverData = await this.httpClient.post('http://localhost:3000/drivers', driver);
+  async create (driver: Driver) {
+    const input: CreateDriverInput = {
+      name: driver.name.getValue(),
+      email: driver.email.getValue(),
+      document: driver.document.getValue(),
+      carPlate: driver.carPlate.getValue(),
+    }
+
+    const driverData = await this.httpClient.post('http://localhost:3000/drivers', input);
     return driverData.driverId;
   }
 }
