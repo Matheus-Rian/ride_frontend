c451b7d64ef4a97d2203d9bba84286be54e3a6dc
Matheus Rian <souzamatheusrian@gmail.com>
2023-08-02 22:23:35 -0300

    feat: create Passenger Builder

--- src/CreatePassenger.vue
+++ src/CreatePassenger.vue
@@ -1,23 +1,26 @@
 <script setup lang="ts">
   import { inject, ref } from 'vue';
   import PassengerGateway from './infra/gateways/PassengerGateway';
-  import Passenger from './domain/Passenger';
+  import { PassengerBuilder } from './domain/Passenger';
 
-  const passenger = ref(new Passenger('', '', '', ''));
-  const passengerId = ref('');
+  const passengerBuilder = ref(new PassengerBuilder());
+  const passenger = ref();
   const passengerGateway = inject('passengerGateway') as PassengerGateway;
 
   async function createPassenger() {
-    passengerId.value  = await passengerGateway.save(passenger.value);
+    passenger.value = passengerBuilder.value.build();
+    passenger.value.passengerId = await passengerGateway.create(passenger.value);
   }
 </script>
 
 <template>
-  <input class="passenger-name" v-model="passenger.name" />
-  <input class="passenger-email" v-model="passenger.email" />
-  <input class="passenger-document" v-model="passenger.document" />
+  <input class="passenger-name" v-model="passengerBuilder.name" />
+  <input class="passenger-email" v-model="passengerBuilder.email" />
+  <input class="passenger-document" v-model="passengerBuilder.document" />
   <button class="create-passenger-button" @click="createPassenger()">Create passenger</button>
-  <div class="passenger-id">{{ passengerId }}</div>
+  <div v-if="passenger">
+    <div class="passenger-id">{{ passenger.passengerId }}</div>
+  </div>
 </template>
 
 <style scoped>
--- src/domain/Passenger.ts
+++ src/domain/Passenger.ts
@@ -1,6 +1,32 @@
+import Cpf from "./Cpf";
+import Email from "./Email";
+import Name from "./Name";
+
 export default class Passenger {
+  passengerId: string;
+  name: Name;
+  email: Email;
+  document: Cpf;
 
-  constructor(readonly passengerId?: string, readonly name?: string, readonly email?: string, readonly document?: string) {
+  constructor(passengerId: string, name: string, email: string, document: string) {
+    this.passengerId = passengerId;
+    this.name = new Name(name);
+    this.email = new Email(email);
+    this.document = new Cpf(document);
+  }
 
+  static create (builder: PassengerBuilder) {
+    return new Passenger(builder.passengerId, builder.name, builder.email, builder.document);
+  }
+}
+
+export class PassengerBuilder {
+  passengerId = '';
+  name = '';
+  email = '';
+  document = '';
+
+  build() {
+    return Passenger.create(this);
   }
 }
